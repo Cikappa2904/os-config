f9e2ba594b4704ea91e5f572395e7ef06c3ea750
Cikappa2904 <49496033+Cikappa2904@users.noreply.github.com>
2022-09-04 17:21:05 +0200

    Uncommented more stuff and added a Windows theme and a font in the repo

--- windows/configurating/WindowsSetup.ps1
+++ windows/configurating/WindowsSetup.ps1
@@ -1,8 +1,8 @@
-param([Parameter(Mandatory=$true)] $JSONFile,[Parameter(Mandatory=$true)] $AHKPath, [Switch]$NoWinget)
+param([Parameter(Mandatory=$true)] $JSONFile,[Parameter()] $AHKPath, [Switch]$NoWinget)
 $username = $env:UserName
 
 #Installare software con Winget
-if ($No -eq $false)
+if ($NoWinget -eq $false)
 {
     $JSONContent = Get-Content $JSONFile | ConvertFrom-Json
 
@@ -20,8 +20,8 @@ if ($No -eq $false)
 Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -name "DisabledHotkeys" -value "S"
 
 #Disattivare ricerca online
-#New-Item -path "HKCU:\Software\Policies\Microsoft\Windows" -Name Explorer 
-#Set-ItemProperty -path HKCU:\Software\Policies\Microsoft\Windows\Explorer -name "DisableSearchBoxSuggestions" -value 1
+New-Item -path "HKCU:\Software\Policies\Microsoft\Windows" -Name Explorer 
+Set-ItemProperty -path HKCU:\Software\Policies\Microsoft\Windows\Explorer -name "DisableSearchBoxSuggestions" -value 1
 
 # Attivare estensioni file (vuole un riavvio perchè boh)
 Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -name "HideFileExt" -value "0"
@@ -35,68 +35,69 @@ Set-ItemProperty -path HKCU:\Software\Microsoft\Clipboard -name "EnableClipboard
 # Mouse snapping sui bottoni default (Ci vuole un logout e su VMware non va perchè boh)
 Set-ItemProperty -path "HKCU:\Control Panel\Mouse" -name "SnapToDefaultButton" -value "1"
 
-# #Cambiare colori
-# Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -name "ColorPrevalence" -value "1" 
-# Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\DWM -name "ColorPrevalence" -value "1" 
-# Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\DWM -name "AccentColor" -value "ffd5eb34" 
-# Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\DWM -name "ColorizationColor" -value "c4680081" 
-#     #Uso AccentColorizer per mostrare il colore in zone dove normalmente non verrebbe mostrato
-# curl -o "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AccentColorizer-x64.exe" "https://github.com/krlvm/AccentColorizer/releases/download/v1.1.12/AccentColorizer-x64.exe" 
+# Attivare conferma eliminazione cestino
+New-Item -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name Explorer 
+Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -name "ConfirmFileDelete" -value 1
+
+#disattivare ibernazione
+powercfg.exe /hibernate off
+
+#Disattivare Precisione puntatore
+Set-ItemProperty -path "HKCU:\Control Panel\Mouse" -name "MouseSpeed" -value 0
+Set-ItemProperty -path "HKCU:\Control Panel\Mouse" -name "MouseThreshold1" -value 0
+Set-ItemProperty -path "HKCU:\Control Panel\Mouse" -name "MouseThreshold2" -value 0
+
+$temp = $env:TEMP 
+#Cambiare colori 
+curl -o $temp "https://github.com/Cikappa2904/os-config/blob/main/windows/configurating/rosso.deskthemepack"
+"$temp\rosso.deskthemepack"
+ 
+#Uso AccentColorizer per mostrare il colore in zone dove normalmente non verrebbe mostrato
+curl -o "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AccentColorizer-x64.exe" "https://github.com/krlvm/AccentColorizer/releases/download/v1.1.12/AccentColorizer-x64.exe" 
 
 # Impostare tema scuro
-#Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -name "SystemUsesLightTheme" -value "0" 
-#Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -name "AppsUseLightTheme" -value "0" 
+Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -name "SystemUsesLightTheme" -value "0" 
+Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -name "AppsUseLightTheme" -value "0" 
 
-
-
-
-
-# #Attivare conferma eliminazione cestino
-# Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer -name "ConfirmFileDelete" -value "1"
-
-
-# #Copiare hotkey di ShareX
-# curl -o "C:\Users\$username\Documents\ShareX\HotkeysConfig.json" "https://gist.githubusercontent.com/Cikappa2904/ae4b75a7ffac3c0ff0e330bd2d58fd42/raw/3268c4655a1bf6f1334bfdcd35c2751b74920680/HotkeysConfig.json"
+#Copiare hotkey di ShareX
+curl -o "C:\Users\$username\Documents\ShareX\HotkeysConfig.json" "https://gist.githubusercontent.com/Cikappa2904/ae4b75a7ffac3c0ff0e330bd2d58fd42/raw/3268c4655a1bf6f1334bfdcd35c2751b74920680/HotkeysConfig.json"
  
-# #disattivare ibernazione
-# powercfg.exe /hibernate off
 
 # #Impostare gli script di AHK all'avvio (https://stackoverflow.com/questions/9701840/how-to-create-a-shortcut-using-powershell)
 
+if ($AHKPath)
+{
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\premiere shortcut.lnk")
+    $Shortcut.TargetPath = "$AHKPath\premiere shortcut.ahk"
+    $Shortcut.Save()
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\windows shortcut.lnk")
+    $Shortcut.TargetPath = "$AHKPath\windows shortcut.ahk"
+    $Shortcut.Save()
 
-# $WshShell = New-Object -comObject WScript.Shell
-# $Shortcut = $WshShell.CreateShortcut("C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\premiere shortcut.lnk")
-# $Shortcut.TargetPath = "$AHKPath\premiere shortcut.ahk"
-# $Shortcut.Save()
-
-# $WshShell = New-Object -comObject WScript.Shell
-# $Shortcut = $WshShell.CreateShortcut("C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\windows shortcut.lnk")
-# $Shortcut.TargetPath = "$AHKPath\windows shortcut.ahk"
-# $Shortcut.Save()
+}
 
 
-# #Installare vari font
-# $temp = $env:TEMP 
+#Installare vari font
 
-# curl -o "$temp\CascadiaCode.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete.otf
-# Copy-Item "$temp\CascadiaCode.otf" "C:\Windows\Temp\Font"
-# Remove-Item "$temp\CascadiaCode.otf"
 
-# curl -o "$temp\FiraCode.ttf" https://1drv.ms/u/s!AroVog_hHY2ngqc8s_n_znpCZ-BSrA?e=E67dIy
-# Copy-Item "$temp\FiraCode.ttf" "C:\Windows\Temp\Font"
-# Remove-Item "$temp\FiraCode.ttf"
-
-# curl -o "$temp\bebasneue.zip" https://dl.dafont.com/dl/?f=bebas_neue
-# mkdir "$temp\BebasNeue"
-# Expand-Archive -LiteralPath "$temp\bebasneue.zip" -DestinationPath "$temp\BebasNeue"
-# Copy-Item "$temp\BebasNeue\BebasNeue-Regular.ttf" "C:\Windows\Temp\Font"
-# Remove-Item "$temp\BebasNeue"
-
-# curl -o "$temp\coolvetica.zip" https://dl.dafont.com/dl/?f=coolvetica
-# mkdir "$temp\coolvetica"
-# Expand-Archive -LiteralPath "$temp\coolvetica.zip" -DestinationPath "$temp\coolvetica"
-# Copy-Item "$temp\coolvetica\coolvetica rg.otf" "C:\Windows\Temp\Font"
-# Remove-Item "$temp\coolvetica"
+curl -o "$temp\CaskaydiaCove.otf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Windows%20Compatible%20Regular.otf"
+Copy-Item "$temp\CaskaydiaCove.otf" "C:\Windows\Temp\Font"
+Remove-Item "$temp\CaskaydiaCove.otf"
+curl -o "$temp\FiraCode.ttf" "https://1drv.ms/u/s!AroVog_hHY2ngqc8s_n_znpCZ-BSrA?e=E67dIy"
+Copy-Item "$temp\FiraCode.ttf" "C:\Windows\Temp\Font"
+Remove-Item "$temp\FiraCode.ttf"
+curl -o "$temp\bebasneue.zip" "https://dl.dafont.com/dl/?f=bebas_neue"
+mkdir "$temp\BebasNeue"
+Expand-Archive -LiteralPath "$temp\bebasneue.zip" -DestinationPath "$temp\BebasNeue"
+Copy-Item "$temp\BebasNeue\BebasNeue-Regular.ttf" "C:\Windows\Temp\Font"
+Remove-Item "$temp\BebasNeue
+curl -o "$temp\coolvetica.zip" "https://dl.dafont.com/dl/?f=coolvetica"
+mkdir "$temp\coolvetica"
+Expand-Archive -LiteralPath "$temp\coolvetica.zip" -DestinationPath "$temp\coolvetica"
+Copy-Item "$temp\coolvetica\coolvetica rg.otf" "C:\Windows\Temp\Font"
+Remove-Item "$temp\coolvetica"
 
 
 # # Impostare tema di oh-my-posh
